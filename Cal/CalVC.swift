b9f38239b6947bee0f39d3e410d965913f02768a
hearther <hearther.lin@gmail.com>
2024-01-23 12:10:52 +0800

    basic operation done TODO: - landscape - overflow handle

--- Cal/CalVC.swift
+++ Cal/CalVC.swift
@@ -9,21 +9,67 @@ import UIKit
 //ios calc
 //Portrait input max 9 digits
 //landscape input  max 16 digits
+//max number 1e161
 
-
-enum Operation {
-    case add, sub, mult, div, none
+extension Formatter {
+    static let withCommaSeparator: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
 }
+extension Double {
+    func formatCommaStr(justTapDec just:Bool = false) -> String {
+        var ret = ""
+        if self == floor(self) && !just{
+            //int
+            ret = Formatter.withCommaSeparator.string(for: Int(self)) ?? ""
+        }
+        else if just == true{
+            let t = Formatter.withCommaSeparator.string(for: Int(self)) ?? ""
+            ret = t+"."
+        }
+        else {
+            ret = String(self)
+        }
+        return ret
+        
+    }
+}
+
+
+typealias BinOp = (Double, Double) -> Double
+//MARK: math functions + - * /
+private func _addFun(a: Double, b: Double) -> Double {
+    let result = a + b
+    return result
+}
+private func _subFun(a: Double, b: Double) -> Double {
+    let result = a - b
+    return result
+}
+private func _mulFun(a: Double, b: Double) -> Double {
+    let result = a * b
+    return result
+}
+private func _divFun(a: Double, b: Double) -> Double {
+    let result = a / b
+    return result
+}
+
+let ops: [String: BinOp] = [ "+" : _addFun, "-" : _subFun, "*" : _mulFun, "/" : _divFun]
+
 
 class CalVC: UIViewController {
     
-    @IBOutlet var ansLabel: UILabel!
+    @IBOutlet var curValLabel: UILabel!
     @IBOutlet var formula: UILabel!
     
     @IBOutlet var ac: UIButton!
     @IBOutlet var neg: UIButton!
     @IBOutlet var per: UIButton!
-        
+    
     @IBOutlet var div: UIButton!
     @IBOutlet var mult: UIButton!
     @IBOutlet var add: UIButton!
@@ -44,11 +90,97 @@ class CalVC: UIViewController {
     
     var btns:Array<UIButton> = []
     var opbtns:Array<UIButton> = []
+        
+    var userInput:String = ""
+    var acc:Double = 0.0
     
-    var val:Int64 = 0
-    var beforeOpNum:Int64 = 0
-    var form = "0"
-    var currentOperation: Operation = .none
+    var numStk: [Double] = [] // Number stack
+    var opStk: [String] = [] // Operator stack
+    
+    private func resetAllDefault (){
+        self.userInput = ""
+        self.acc = 0.0
+        
+        self.numStk.removeAll()
+        self.opStk.removeAll()
+    }
+
+    private func handleInput(_ input:String){
+        if input == "-"{
+            if self.userInput.hasPrefix("-"){
+                self.userInput = String(self.userInput[self.userInput.index(after: self.userInput.startIndex)...])
+            }
+            else {
+                self.userInput = input + self.userInput
+            }
+        }
+        else {
+            self.userInput += input
+        }
+        acc = Double((self.userInput as NSString).doubleValue)
+        updateUI(input==".")
+    }
+    private func updateUI(_ justInputDec:Bool = false){
+        
+        guard !self.acc.isInfinite && !self.acc.isNaN else {
+            self.curValLabel.text = "ERR"
+            self.resetAllDefault()
+            return
+        }
+                
+        var str = self.acc.formatCommaStr()
+        //Portrait input max 9 digits
+        //landscape input  max 16 digits
+        if str.count >= 9 {
+            return
+        }
+        if justInputDec{
+            str += "."
+        }
+        self.curValLabel.text = str
+    }
+    private func doOp(_ newop:String){
+        if self.userInput != "" {
+            //%
+            if (newop == "%"){
+                self.acc /= 100
+                doEq()
+            }
+            else if self.numStk.count > 0 && self.opStk.count > 0
+            {
+                let lastOp = self.opStk.last
+                // first * /  then + -
+                if !((newop == "*" || newop == "/") && (lastOp == "+" || lastOp == "-")) {
+                    let fun = ops[self.opStk.removeLast()]
+                    self.acc = fun!(self.numStk.removeLast(), acc)
+                    doEq()
+                }
+            }
+        }
+        self.opStk.append(newop)
+        self.numStk.append(acc)
+        self.userInput = ""
+        
+        updateUI()
+    }
+    private func doEq() {
+        if self.userInput == ""{
+            return
+        }
+        if numStk.count>0 {
+            let lastOp = self.opStk.last
+            
+            if lastOp != nil {
+                if ops.keys.contains(lastOp!){
+                    let fun = ops[self.opStk.removeLast()]
+                    self.acc = fun!(self.numStk.removeLast(), acc)
+                    doEq()
+                }
+            }
+        }
+        updateUI()
+        self.userInput = ""
+    }
             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,83 +207,72 @@ class CalVC: UIViewController {
     @IBAction func onTap(_ sender: UIButton){
         if self.opbtns.contains(sender){
             if sender == self.add {
-                self.currentOperation = .add
+                doOp("+")
             }
             else if sender == self.sub {
-                self.currentOperation = .sub
+                doOp("-")
             }
             else if sender == self.mult {
-                self.currentOperation = .mult
+                doOp("*")
             }
             else if sender == self.div {
-                self.currentOperation = .div
+                doOp("/")
             }
             
-            self.val = 0
-            self.beforeOpNum = self.val
         }
         else if sender == self.eq {
-            let runningValue = self.beforeOpNum
-            let currentValue:Int64 = self.val
-            switch self.currentOperation {
-            case .add:
-                self.val = runningValue + currentValue
-                self.form = "\(runningValue)+\(currentValue)"
-            case .sub:
-                self.val = runningValue - currentValue
-                self.form = "\(runningValue)-\(currentValue)"
-            case .mult:
-                self.val = runningValue * currentValue
-                self.form = "\(runningValue)*\(currentValue)"
-            case .div:
-                self.val = runningValue / currentValue
-                self.form = "\(runningValue)/\(currentValue)"
-            case .none:
-                break
-            }
-            self.formula.text = self.form
+            doEq()
+//            let runningValue = self.beforeOpNum
+//            let currentValue:Double = self.val
+//            switch self.currentOperation {
+//            case .add:
+//                self.val = runningValue + currentValue
+//                self.form = "\(runningValue)+\(currentValue)"
+//            case .sub:
+//                self.val = runningValue - currentValue
+//                self.form = "\(runningValue)-\(currentValue)"
+//            case .mult:
+//                self.val = runningValue * currentValue
+//                self.form = "\(runningValue)*\(currentValue)"
+//            case .div:
+//                self.val = runningValue / currentValue
+//                self.form = "\(runningValue)/\(currentValue)"
+//            case .none:
+//                break
+//            }
+//            self.formula.text = self.form
         }
         else if sender == self.ac {
-            self.val = 0
-            self.form = "0"
-            self.formula.text = self.form
+            self.resetAllDefault()
+            updateUI()
         }
         else if sender == self.neg {
-            self.val *= -1
-            self.ansLabel.text = String(self.val)
+            handleInput("-")
         }
         else if sender == self.dec {
-            
-        }
-        else if sender == self.per {
-            
-        }
-        else {
-            
-            let d = sender.tag
-            if self.val > 10^9 {
+            if self.userInput.firstIndex(of: ".") != nil {
                 return
             }
-            
-            if self.val == 0 {
-                val = Int64(d)
-            }
-            else {
-                self.val = val * 10 + Int64(d)
-            }
-            
-            
-            if self.val == Int64.max {
-                self.ansLabel.text = "INF"
-            }
-            else if self.val == Int64.min{
-                self.ansLabel.text = "-INF"
-            }
-            else {
-                self.ansLabel.text = String(self.val)
-            }
-            
+            handleInput(".")
+        }
+        else if sender == self.per {
+            doOp("%")
+        }
+        else {
+            // 0 1 2 3 4 5 6 7 8 9
+            let d = sender.tag
+            handleInput(String(d))
+        }
+        
+        if self.numStk.count > 0{
+            self.ac.titleLabel?.text = "C"
+        }
+        else {
+            self.ac.titleLabel?.text = "AC"
         }
         
     }
+    //MARK: private
+    
+    
 }
